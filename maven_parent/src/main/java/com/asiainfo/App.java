7108f021d92d2591e6ec5023bd7114b73a9115aa
王建凯亚信 <2717927533@qq.com>
2018-12-11 10:05:04 +0800

    branch3修改

--- maven_parent/src/main/java/com/asiainfo/App.java
+++ maven_parent/src/main/java/com/asiainfo/App.java
@@ -9,10 +9,12 @@ public class App
     public static void main( String[] args ){
 
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
+
         System.out.println( "Hello World!" );
 
         System.out.println("20181210建立3.0分支");
+
+        System.out.println("branch3.0"+111);
+        System.out.println("branch3.0"+222);
     }
 }
